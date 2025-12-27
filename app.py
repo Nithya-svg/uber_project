a8efe5ab3ae3506474d29b7febd009b76ac01905
Nithya-svg <nithyavarshini0504@gmail.com>
2025-12-27 20:52:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+from flask import Flask, render_template,request
+import math
+import pickle
+app = Flask(__name__)
+model2 = pickle.load(open('model.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [ int(i) for i in request.form.values()]
+    final_features = np.array(int_features).reshape(1,-1)
+    prediction = model2.predict(final_features)
+    output = round(prediction[0],2)
+    return render_template('index.html',predict_text='Number of Weekly Riders is {}'.format(math.floor(output)))
+if __name__ == "__main__":
+    app.run(debug = True)
